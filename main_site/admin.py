0d5057bb3a030474dc4084f3532d05371f1a9e17
twsomt <volk0v-ak@yandex.ru>
2022-10-05 21:00:16 +0300

    Register model

--- main_site/admin.py
+++ main_site/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main_site.models import Articles
 
 # Register your models here.
+
+@admin.register(Articles)
+class ArticlesAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'content')
