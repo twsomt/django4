f564936cb6f6b2ff6a773ef6d2a48a9fe029e161
twsomt <volk0v-ak@yandex.ru>
2022-10-12 21:44:49 +0300

    HTML OK

--- main_site/models.py
+++ main_site/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Articles(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
+    slug = models.SlugField()
     #template_name = ''
 
     def __str__(self):
